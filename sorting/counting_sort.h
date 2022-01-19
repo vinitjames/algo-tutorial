808369eda896c763ae47b4992ee004ea65f9214e
vinit <vinit.james24@gmail.com>
2022-01-19 16:16:52 +0100

    modified counting sort for radix sort with any base

--- sorting/counting_sort.h
+++ sorting/counting_sort.h
@@ -4,17 +4,18 @@
 #include <algorithm>
 #include <vector>
 
-inline void counting_sort(int* arr, const std::size_t len){
+inline void counting_sort(int* arr, const std::size_t& len,
+						  const int& digit, const int& base){
 	if (arr == nullptr)
 		return;
-	int max = *std::max_element(arr, arr+len);
-	std::vector<int> pos_arr(max+1);
+	std::vector<int> pos_arr(base);
 	std::fill(pos_arr.begin(), pos_arr.end(), 0);
+	unsigned long divisor = pow(base, digit);  
 	for (int i=0; i<len; i++)
-		pos_arr[arr[i]]++;
+		pos_arr[(arr[i]/divisor)%base]++;
 	int count_sum = pos_arr[0];
 	pos_arr[0] = 0;
-	for (int i = 1; i<=max ; i++){
+	for (int i = 1; i<base ; i++){
 	 	int temp = pos_arr[i];
 		pos_arr[i] = pos_arr[i-1] + count_sum;
 		count_sum = temp;
@@ -22,10 +23,13 @@ inline void counting_sort(int* arr, const std::size_t len){
 	int temp_arr[len];
 	std::copy(arr, arr+ len, temp_arr);
 	for(int i=0; i<len; i++){
-		arr[pos_arr[temp_arr[i]]] = temp_arr[i];
-		pos_arr[temp_arr[i]]++;
+		unsigned long digit_of_base = (temp_arr[i]/divisor)%base;
+		arr[pos_arr[digit_of_base]] = temp_arr[i];
+		pos_arr[digit_of_base]++;
 	}
 }
+
+
 
 #endif /* COUNTING_SORT_H */
 
---
+++ sorting/radix_sort.h
@@ -0,0 +1,21 @@
+#ifndef RADIX_SORT_H
+#define RADIX_SORT_H
+
+#include <algorithm>
+#include <math.h>
+#include "counting_sort.h"
+
+
+inline void radix_sort(int* arr, const std::size_t& len, const int& radix){
+	if(arr == nullptr)
+		return;
+	int max = *std::max_element(arr, arr+len);
+	int digits = int(log(max)/log(radix) + 1.0);
+
+	for(int i=0; i<digits; i++){
+		counting_sort(arr, len, i, radix);
+	}
+		
+}
+
+#endif /* RADIX_SORT_H */
